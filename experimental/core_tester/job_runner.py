a0be573a57d9c870ce20d5053d7048d3bb258c61
zav <zav@cherre.com>
2020-02-06 12:03:32 -0500

    Added log watcher

--- experimental/core_tester/job_runner.py
+++ experimental/core_tester/job_runner.py
@@ -6,78 +6,11 @@ from time import sleep
 from typing import List
 import logging
 from datetime import datetime
+from readers import ThreadedKuebrnetesLogReader
 
 logging.basicConfig(level="INFO")
 
 CUR_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
-
-
-class LogWatch(kubernetes.watch.Watch):
-    @staticmethod
-    def iter_resp_lines(resp):
-        prev = ""
-        for seg in resp.read_chunked(decode_content=False):
-            if isinstance(seg, bytes):
-                seg = seg.decode("utf8")
-            seg = prev + seg
-            lines = seg.split("\n")
-            if not seg.endswith("\n"):
-                prev = lines[-1]
-                lines = lines[:-1]
-            else:
-                prev = ""
-            for line in lines:
-                if line:
-                    yield line
-
-    def stream(self, func, *args, **kwargs):
-        """Watch an API resource and stream the result back via a generator.
-
-        :param func: The API function pointer. Any parameter to the function
-                     can be passed after this parameter.
-
-        :return: Event object with these keys:
-                   'type': The type of event such as "ADDED", "DELETED", etc.
-                   'raw_object': a dict representing the watched object.
-                   'object': A model representation of raw_object. The name of
-                             model will be determined based on
-                             the func's doc string. If it cannot be determined,
-                             'object' value will be the same as 'raw_object'.
-
-        Example:
-            v1 = kubernetes.client.CoreV1Api()
-            watch = kubernetes.watch.Watch()
-            for e in watch.stream(v1.list_namespace, resource_version=1127):
-                type = e['type']
-                object = e['object']  # object is one of type return_type
-                raw_object = e['raw_object']  # raw_object is a dict
-                ...
-                if should_stop:
-                    watch.stop()
-        """
-
-        self._stop = False
-        return_type = self.get_return_type(func)
-        kwargs["watch"] = True
-        kwargs["_preload_content"] = False
-        if "resource_version" in kwargs:
-            self.resource_version = kwargs["resource_version"]
-
-        timeouts = "timeout_seconds" in kwargs
-        while True:
-            resp = func(*args, **kwargs)
-            try:
-                for line in LogWatch.iter_resp_lines(resp):
-                    yield line
-                    if self._stop:
-                        break
-            finally:
-                kwargs["resource_version"] = self.resource_version
-                resp.close()
-                resp.release_conn()
-
-            if timeouts or self._stop:
-                break
 
 
 def load_raw_formatted_file(fpath):
@@ -100,12 +33,11 @@ kubernetes.config.load_kube_config()
 contexts, active_context = kubernetes.config.list_kube_config_contexts()
 current_namespace = active_context["context"]["namespace"]
 client = kubernetes.client.CoreV1Api()
-watcher = LogWatch()
 
 bash_script = """
 echo "Starting"
 while true; do
-    date
+    echo $((`date +%s`*1000+`date +%-N`/1000000))
     sleep 1
 done
 """
@@ -115,30 +47,15 @@ pod_to_execute = create_pod_v1_object("lama", "ubuntu", ["bash", "-c", bash_scri
 print("Validate the pod...")
 try:
     status = client.read_namespaced_pod_status("lama", current_namespace)
-except:
+except Exception:
     print("Pod dose not exist creating...")
     client.create_namespaced_pod(current_namespace, pod_to_execute)
     sleep(10)
 
 print("Watch...")
 
+reader = ThreadedKuebrnetesLogReader(client, "lama", current_namespace)
+reader.on("log", lambda msg: logging.info(msg))
+reader.start()
+reader.join()
 
-def read_log(*args, **kwargs):
-    val = client.read_namespaced_pod_log_with_http_info(
-        "lama", current_namespace, _preload_content=False, follow=True
-    )
-    return val[0]
-
-
-started = datetime.now()
-for log_line in watcher.stream(read_log):
-    logging.info(log_line)
-    running_for = datetime.now() - started
-    if running_for.seconds > 30:
-        watcher.stop()
-
-
-# for event in watcher.stream(read_log):
-#     logging.log(event)
-
-kubernetes.utils.create_from_yaml
--- experimental/core_tester/readers.py
+++ experimental/core_tester/readers.py
@@ -3,6 +3,62 @@ import threading
 from threading import Thread
 
 
+class KubernetesWatchRawStream(kubernetes.watch.Watch):
+    @staticmethod
+    def iter_resp_lines(resp):
+        prev = ""
+        for seg in resp.read_chunked(decode_content=False):
+            if isinstance(seg, bytes):
+                seg = seg.decode("utf8")
+            seg = prev + seg
+            lines = seg.split("\n")
+            if not seg.endswith("\n"):
+                prev = lines[-1]
+                lines = lines[:-1]
+            else:
+                prev = ""
+            for line in lines:
+                if line:
+                    yield line
+
+    def stream(self, func, *args, **kwargs):
+        """Watch an API resource and stream the result back via a generator.
+
+        :param func: The API function pointer. Any parameter to the function
+                     can be passed after this parameter.
+
+        :return: Event object with these keys:
+                   'type': The type of event such as "ADDED", "DELETED", etc.
+                   'raw_object': a dict representing the watched object.
+                   'object': A model representation of raw_object. The name of
+                             model will be determined based on
+                             the func's doc string. If it cannot be determined,
+                             'object' value will be the same as 'raw_object'.
+        """
+
+        self._stop = False
+        kwargs["watch"] = True
+        kwargs["_preload_content"] = False
+        if "resource_version" in kwargs:
+            self.resource_version = kwargs["resource_version"]
+
+        timeouts = "timeout_seconds" in kwargs
+        while True:
+            resp = func(*args, **kwargs)
+            try:
+                for line in KubernetesWatchRawStream.iter_resp_lines(resp):
+                    yield line
+                    if self._stop:
+                        break
+            finally:
+                kwargs["resource_version"] = self.resource_version
+                resp.close()
+                resp.release_conn()
+
+            if timeouts or self._stop:
+                break
+
+
 class EventHandler:
     message_handlers: dict = None
 
@@ -36,15 +92,14 @@ class ThreadedKuebrnetesLogReader(EventHandler):
     _active_log_read_thread: Thread = None
     _is_stopped: bool = False
 
-    def __init__(self, client, pod_name, namespace, on_message):
+    def __init__(self, client, pod_name, namespace):
         super().__init__()
-        self.on_message = on_message
         self.client = client
         self.pod_name = pod_name
         self.namespace = namespace
 
     def start(self):
-        if self.active_log_read_thread is not None:
+        if self._active_log_read_thread is not None:
             raise Exception("Log reader has already been started.")
 
         def read_log(*args, **kwargs):
@@ -53,18 +108,29 @@ class ThreadedKuebrnetesLogReader(EventHandler):
             )
             return val[0]
 
-        def log_reader(reader: ThreadedKuebrnetesLogReader):
-            for log_line in kubernetes.watch.Watch().stream(read_log):
-                self.emit("log", msg=log_reader)
+        def log_reader():
+            for log_line in KubernetesWatchRawStream().stream(read_log):
+                self.emit("log", msg=log_line)
+                if self._is_stopped:
+                    break
+            self._active_log_read_thread = None
 
-        self._active_log_read_thread = threading.Thread(target=log_reader, args=(self))
+        self._is_stopped = False
+        self._active_log_read_thread = threading.Thread(target=log_reader)
+        self._active_log_read_thread.start()
 
     def stop(self):
         self._is_stopped = True
 
     def abort(self):
         if (
-            self.active_log_read_thread is not None
-            and self.active_log_read_thread.isAlive()
+            self._active_log_read_thread is not None
+            and self._active_log_read_thread.isAlive()
         ):
             self._active_log_read_thread._stop()
+
+    def join(self, timeout: float = None):
+        if self._active_log_read_thread is None:
+            raise Exception("Logger must be started before it can be joined")
+
+        self._active_log_read_thread.join(timeout)
