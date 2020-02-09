b371901a261d437f5475256800b94b1fe5110e89
Zav <zav@cherre.com>
2020-02-08 20:08:57 -0500

    linter

--- src/kubernetes_job_operator.py
+++ src/kubernetes_job_operator.py
@@ -1,8 +1,7 @@
 from airflow.utils.decorators import apply_defaults
 from airflow.exceptions import AirflowException
 from airflow.operators import BaseOperator
-from airflow.operators.bash_operator import BashOperator
-
+# from airflow.operators.bash_operator import BashOperator
 # from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 # from airflow.contrib.kubernetes import kube_client
 from .job_runner import JobRunner
