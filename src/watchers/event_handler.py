639f66e79284bc6962133eaa3cc586ba5096efd4
Zav <zav@cherre.com>
2020-02-12 10:17:34 -0500

    Added help for the event handler

--- src/watchers/event_handler.py
+++ src/watchers/event_handler.py
@@ -7,12 +7,26 @@ class EventHandler:
     _handler_last_idx = 0
 
     def __init__(self):
+        """An event handler object, that can
+        collect/pipe/emit events by name, with arguments.
+        """
         super().__init__()
         self._pipeto = []
         self.message_handlers = dict()
         self._handler_last_idx = 0
 
-    def on(self, name, handler):
+    def on(self, name: str, handler: callable) -> int:
+        """Add a new event call.
+        
+        Arguments:
+            name {str} -- The event name
+            handler {callable} -- The mmethod/other to
+            be called when the event is triggered.
+        
+        Returns:
+            int -- The event index, within the specific name 
+            dictionary. name+index are unique.
+        """
         if not self.hasEvent(name):
             self.message_handlers[name] = dict()
         idx = self._handler_last_idx
@@ -21,17 +35,51 @@ class EventHandler:
         self.message_handlers[name][idx] = handler
         return idx
 
-    def hasEvent(self, name):
+    def hasEvent(self, name: str, index: int = None):
+        """Returns true if the event is present.
+        
+        Arguments:
+            name {str} -- The name of the event
+        
+        Keyword Arguments:
+            index {int} -- If not None, will search for a specific
+            call of this event, according to the event index returned
+            from the on method. (default: {None})
+        
+        Returns:
+            bool -- True if event exists.
+        """
+        if index is not None:
+            return (
+                name in self.message_handlers and index in self.message_handlers[name]
+            )
         return name in self.message_handlers
 
-    def clear(self, name, idx: int = None):
+    def clear(self, name: str, idx: int = None):
+        """Clears an event.
+        
+        Arguments:
+            name {str} -- The name of the event to clear.
+        
+        Keyword Arguments:
+            idx {int} -- If not None, will clear the specific
+            call to the event identified by the event index
+            returned from the on method. (default: {None})
+        """
         if self.hasEvent(name):
             if idx is not None:
                 del self.message_handlers[name][idx]
             else:
                 del self.message_handlers[name]
 
-    def emit(self, name, *args, **kwargs):
+    def emit(self, name: str, *args, **kwargs):
+        """Emits the event to all the event handler
+        callables. Any arguments sent after name, will
+        be passed to the event handler.
+        
+        Arguments:
+            name {str} -- The name of the event to emit.
+        """
         if self.hasEvent(name):
             cur_handlers = list(self.message_handlers[name].values())
             for handler in cur_handlers:
@@ -40,5 +88,11 @@ class EventHandler:
             evnet_handler.emit(name, *args, **kwargs)
 
     def pipe(self, other):
+        """Pipe all events emitted from this hanlder
+        to anohter handler. This action cannot be reverted.
+        
+        Arguments:
+            other {EventHandler} -- The event handler to pipe to.
+        """
         assert isinstance(other, EventHandler)
         self._pipeto.append(other)
