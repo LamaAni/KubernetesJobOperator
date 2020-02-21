e9147c3f7543e9ee8d7165582fa93f874b58f74f
Zav Shotan <zav.shotan@h1insights.com>
2020-02-20 19:25:36 -0500

    Added proper version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name="airflow_kubernetes_job_operator",
-    version="0.2.4",
+    version="0.2.5",
     description="An airflow job operator that executes a task as a Kubernetes job on a cluster, given a job yaml configuration or an image uri.",
     long_description="Please see readme.md",
     classifiers=[],
