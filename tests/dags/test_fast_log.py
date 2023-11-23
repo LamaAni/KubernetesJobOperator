6db038ec3657c16fa943456b4d5d9f3da09b9684
zav shotan <zav.shotan@gmail.com>
2023-11-22 19:04:39 -0500

    --allow-empty

---
+++ tests/dags/test_fast_log.py
@@ -0,0 +1,37 @@
+from utils import default_args, name_from_file
+from airflow import DAG
+from airflow_kubernetes_job_operator.kubernetes_job_operator import (
+    KubernetesJobOperator,
+)
+
+dag = DAG(
+    name_from_file(__file__),
+    default_args=default_args,
+    description="Test base job operator",
+    schedule_interval=None,
+    catchup=False,
+)
+
+namespace = None
+
+envs = {
+    "PASS_ARG": "a test",
+    "JINJA_ENV": "{{ ds }}",
+}
+
+default_delete_policy = "IfSucceeded"
+
+# Job
+KubernetesJobOperator(
+    task_id="test-pod-success",
+    namespace=namespace,
+    body_filepath="./templates/test_job.fast_log.yaml",
+    envs=envs,
+    dag=dag,
+    delete_policy=default_delete_policy,
+)
+
+if __name__ == "__main__":
+    from tests.test_utils import test_dag
+
+    test_dag(dag)
